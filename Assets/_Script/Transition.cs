8049844eaf0f6e4e2dc3e25a1c43437c4c02b09c
nopparut14546-collab <nopparut.14546@gmail.com>
2025-11-30 00:05:49 +0700

    Scene Transition

---
+++ Assets/_Script/Transition.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene("Map2");
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
