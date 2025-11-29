c9599de69a00787e4219663867182ed357088b78
nopparut14546-collab <nopparut.14546@gmail.com>
2025-11-29 20:30:14 +0700

    Movement Left/Right
    Working

---
+++ Assets/_Script/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        rb.linearVelocity = new Vector2(moveHorizontal, rb.linearVelocity.y);
+        Debug.Log(moveHorizontal);
+
+        if (moveHorizontal != 0)
+        {
+            spriteRenderer.flipX = moveHorizontal > 0;
+        }
+    }
+}
